28f4476a50108e76174cb103a5ec6ed98b712317
Abel Watson <80369480+abelwatson@users.noreply.github.com>
2021-07-17 13:06:15 -0400

    adding to abel branch

--- dndclient/src/App.js
+++ dndclient/src/App.js
@@ -1,10 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import AuthLogin from './components/auth/AuthLogin';
 import AuthCreate from './components/auth/AuthCreate';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import CharacterIndex from './components/character/CharacterIndex';
 import './App.css';
-import CharacterIndex from './components/character/CharacterIndex';
 
 
 function App() {
@@ -24,23 +23,35 @@ function App() {
 
   const protectedViews = () => {
     return (
-      sessionToken === localStorage.getItem('token') ? <CharacterIndex token={sessionToken}/> : <AuthLogin updateToken={updateToken}/>
-      )
+      sessionToken === localStorage.getItem('token') ? <CharacterIndex token={sessionToken} /> : <AuthLogin updateToken={updateToken} />
+    )
   }
 
   return (
+    <Router>
+      <div className="App" >
+        <ul>
+          <li >
+            <Link to="/" />
+          </li>
+          <li>
+            <Link to="/SignUp" />
+          </li>
+          <li>
+            {protectedViews()}
+          </li>
+        </ul>
+        </div>
 
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-      <div className='main'>
-      <CharacterIndex />
-      </div>
-      <AuthLogin />
-      {protectedViews()}
-
-    </div>
+      <Switch>
+        <Route path="/Login">
+          <AuthLogin />
+        </Route>
+        <Route path="/SignUp">
+          <AuthCreate />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 

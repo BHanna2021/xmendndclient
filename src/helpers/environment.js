e50776224e418d016685dec1b430b3a1c0d77ad4
Abel Watson <80369480+abelwatson@users.noreply.github.com>
2021-07-23 16:56:22 -0400

    added https to url

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-    case 'aw-bh-jz-dndcreatorclient.herokuapp.com':
+    case 'https://aw-bh-jz-dndcreatorclient.herokuapp.com/':
         APIURL = 'https://aw-bh-jz-dndcreator.herokuapp.com'
 }
 

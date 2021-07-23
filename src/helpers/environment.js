4a1c78e36ad187a6859d7c832e0b32098371fdcc
Abel Watson <80369480+abelwatson@users.noreply.github.com>
2021-07-23 17:53:44 -0400

    removed https://

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-    case 'https://aw-bh-jz-dndcreatorclient.herokuapp.com/':
+    case 'aw-bh-jz-dndcreatorclient.herokuapp.com':
         APIURL = 'https://aw-bh-jz-dndcreator.herokuapp.com'
 }
 

8e3c79dec4d33e9ace04f948fbc1ac08bb91cd2a
jlzimski <jlzimski@gmail.com>
2021-07-26 23:31:38 -0400

    partially working carousel and very tired eyes

--- src/components/character/CharacterCarousel.js
+++ src/components/character/CharacterCarousel.js
@@ -1,62 +1,51 @@
 import React from 'react';
 // import './App.css';
-// import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from 'reactstrap';
 // import "react-responsive-carousel/lib/styles/carousel.min.css"; // ???
 
-const CharacterCarousel = (props) => {
-
+const CharacterCarousel = () => {
     return (
-        <div className="main">
-        <div className="mainDiv">
-            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="./carouselbackgound1.jpg" class="d-block w-100" alt="..."></img>
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
-                            </div>
-                    </div>
-                    <div class="carousel-item active">
-                        <img src="carouselbackgound2.jpg" class="d-block w-100" alt="..."></img>
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
-                            </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="carouselbackground3.jpg" class="d-block w-100" alt="..."></img>
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"  aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"  aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="Carouselbackground1.jpg" class="d-block w-100" alt="..."></img>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h4>Creating A Character</h4>
+                            <h5>First: Select Your Race and Class</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+            </div>
+                    <div class="carousel-item ">
+                        <img src="Carouselbackground2.jpg" class="d-block w-100" alt="..."></img>
                             <div class="carousel-caption d-none d-md-block">
                                 <h5>Second slide label</h5>
                                 <p>Some representative placeholder content for the second slide.</p>
                             </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="carouselbackground4.jpg" class="d-block w-100" alt="..."></img>
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>Third slide label</h5>
-                                <p>Some representative placeholder content for the third slide.</p>
-                    </div>
-                </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+                        <div class="carousel-item">
+                            <img src="Carouselbackground3.jpg" class="d-block w-100" alt="..."></img>
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Third slide label</h5>
+                                    <p>Some representative placeholder content for the third slide.</p>
+                                </div>
             </div>
-        </div>
-    </div>
-
-    );
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                    )
 }
 
 export default CharacterCarousel;

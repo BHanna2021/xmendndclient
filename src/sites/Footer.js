d112a9f39555e7e6d11a75da8df62dda35a8586c
Abel Watson <80369480+abelwatson@users.noreply.github.com>
2021-07-27 18:08:46 -0400

    more footer links

--- src/sites/Footer.js
+++ src/sites/Footer.js
@@ -27,10 +27,15 @@ const Footer = () => {
                     </Column>
                     <Column>
                         <Heading>Resources</Heading>
-                        <FooterLink style={{ fontSize: 13 }} href="http://dnd5e.wikidot.com/" target="_blank"> DnD 5e WikiDot </FooterLink>
-                        <FooterLink style={{ fontSize: 13 }} href="http://rolladvantage.com/tokenstamp/" target="_blank"> Token Creator </FooterLink>
-                        <FooterLink style={{ fontSize: 13 }} href="https://app.roll20.net/" target="_blank"> Roll20 Online Play </FooterLink>
-                        <FooterLink style={{ fontSize: 13 }} href="https://dicegrimorium.com/free-rpg-map-library/" target="_blank"> Free Digital Maps </FooterLink>
+                        <FooterLink href="http://dnd5e.wikidot.com/" target="_blank"> DnD 5e WikiDot </FooterLink>
+                        <FooterLink href="http://rolladvantage.com/tokenstamp/" target="_blank"> Token Creator </FooterLink>
+                        <FooterLink href="https://app.roll20.net/" target="_blank"> Roll20 Online Play </FooterLink>
+                    </Column>
+                    <Column>
+                        <Heading>Resources</Heading>
+                        <FooterLink href="https://dnd.wizards.com/" target="_blank">Dungeons &amp; Dragons</FooterLink>
+                        <FooterLink href="https://dicegrimorium.com/free-rpg-map-library/" target="_blank"> Free Digital Maps </FooterLink>
+                        <FooterLink href="https://www.dndbeyond.com/" target="_blank"> D&amp;D Beyond </FooterLink>
                     </Column>
                     <Column>
                         <Heading>Issues</Heading>
--- src/sites/Header.js
+++ src/sites/Header.js
@@ -5,12 +5,14 @@ const ScrollingBanner = () => {
         <div id="BannerContainer" style={{backgroundColor: "bisque"}}>
             <logo-slider>
                 <div>
+                    <img src="./assets/DMToken" alt="" />
                     <img src="./assets/BarbarianToken.png" alt="" />
                     <img src="./assets/BardToken.png" alt="" />
                     <img src="./assets/ClericToken.png" alt="" />
                     <img src="./assets/DruidToken.png" alt="" />
                     <img src="./assets/FighterToken.png" alt="" />
                     <img src="./assets/MonkToken.png" alt="" />
+                    <img src="./assets/PaladinToken.png" alt="" />
                     <img src="./assets/RangerToken.png" alt="" />
                     <img src="./assets/RogueToken.png" alt="" />
                     <img src="./assets/SorcererToken.png" alt="" />
